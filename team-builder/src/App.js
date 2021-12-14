e07faaead51854923a421ca0074e7f4d5ac2f4c4
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2021-12-14 11:17:54 -0800

    updated index, app.js, teamform

---
+++ team-builder/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TeamForm from './TeamForm';
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: ''
+}
+
+function App() {
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue
+    });
+  }
+
+  const submitForm = () => {
+    const newTeamMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+  }
+
+  // AXIOS????
+
+  return (
+    <div className="App">
+      <h1>Team Builder App</h1>
+      <TeamForm 
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+      />
+
+      {
+        teamMembers.map(teamMember => {
+          return (
+            <TeamMember key={teamMember.id} details={teamMember}/>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
